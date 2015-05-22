a889c26170be649790d953ab80d620bb7b88e7d6
Tuomas Sivula <tuomas.sivula@aalto.fi>
2015-05-22 13:56:19 +0300

    Clean up demo5_1 codes

--- demos_ch5/demo5_1.py
+++ demos_ch5/demo5_1.py
@@ -12,20 +12,24 @@ from scipy.special import gammaln
 import matplotlib.pyplot as plt
 
 # edit default plot settings
-import matplotlib
-matplotlib.rc('font', size=14)
+plt.rc('font', size=14)
+plt.rc('lines', color=(0.3,0.5,0.8), linewidth=2)
+plt.rc('axes', color_cycle=(plt.rcParams['lines.color'],)) # Disable color cycle
 
 # rat data (BDA3, p. 102)
 y = np.array([
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 
-    2, 2, 2, 2, 2, 2, 1, 5, 2, 5, 3, 2, 7, 7, 3, 3, 2, 9, 10, 4, 4, 4, 4, 4, 4, 
-    4, 10, 4, 4, 4, 5, 11, 12, 5, 5, 6, 5, 6, 6, 6, 6, 16, 15, 15, 9, 4
-])    
+     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,
+     1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  1,  5,  2,
+     5,  3,  2,  7,  7,  3,  3,  2,  9, 10,  4,  4,  4,  4,  4,  4,  4,
+    10,  4,  4,  4,  5, 11, 12,  5,  5,  6,  5,  6,  6,  6,  6, 16, 15,
+    15,  9,  4
+])
 n = np.array([
-    20, 20, 20, 20, 20, 20, 20, 19, 19, 19, 19, 18, 18, 17, 20, 20, 20, 20, 19, 
-    19, 18, 18, 25, 24, 23, 20, 20, 20, 20, 20, 20, 10, 49, 19, 46, 27, 17, 49, 
-    47, 20, 20, 13, 48, 50, 20, 20, 20, 20, 20, 20, 20, 48, 19, 19, 19, 22, 46, 
-    49, 20, 20, 23, 19, 22, 20, 20, 20, 52, 46, 47, 24, 14
+    20, 20, 20, 20, 20, 20, 20, 19, 19, 19, 19, 18, 18, 17, 20, 20, 20,
+    20, 19, 19, 18, 18, 25, 24, 23, 20, 20, 20, 20, 20, 20, 10, 49, 19,
+    46, 27, 17, 49, 47, 20, 20, 13, 48, 50, 20, 20, 20, 20, 20, 20, 20,
+    48, 19, 19, 19, 22, 46, 49, 20, 20, 23, 19, 22, 20, 20, 20, 52, 46,
+    47, 24, 14
 ])
 M = len(y)
 
@@ -33,27 +37,23 @@ M = len(y)
 # plot separate and pooled model
 plt.figure(figsize=(8,10))
 x = np.linspace(0, 1, 250)
-blue = (0.3,0.5,0.8)
 
 # separate
 plt.subplot(2, 1, 1)
-for i in range(M-1):
-    line1, = plt.plot(x, beta.pdf(x, y[i] + 1, n[i] - y[i] + 1), color=blue)
-# emphasise last sample
-line2, = plt.plot(
-    x, beta.pdf(x, y[-1] + 1, n[-1] - y[-1] + 1), 'r', linewidth=2
-)
-plt.legend((line1, line2),
+lines = plt.plot(x, beta.pdf(x[:,None], y[:-1] + 1, n[:-1] - y[:-1] + 1),
+                 linewidth=1)
+# highlight the last line
+line1, = plt.plot(x, beta.pdf(x, y[-1] + 1, n[-1] - y[-1] + 1), 'r')
+plt.legend((lines[0], line1),
            ('Posterior of $\\theta_j$', 'Posterior of $\\theta_{71}$'))
 plt.yticks(())
 plt.title('separate model')
 
 # pooled
 plt.subplot(2, 1, 2)
-line1, = plt.plot(
-    x, beta.pdf(x, y.sum() + 1, n.sum() - y.sum() + 1), color=blue, linewidth=2
-)
-plt.legend((line1,), ('Posterior of common $\\theta$',))
+plt.plot(x, beta.pdf(x, y.sum() + 1, n.sum() - y.sum() + 1),
+         linewidth=2, label=('Posterior of common $\\theta$'))
+plt.legend()
 plt.yticks(())
 plt.xlabel('$\\theta$', fontsize=20)
 plt.title('pooled model')
@@ -105,8 +105,7 @@ samp_B += (np.random.rand(nsamp) - 0.5) * (B[1]-B[0])
 # that is, plot Beta(alpha,beta) using posterior samples of alpha and beta
 fig = plt.figure(figsize=(8,10))
 plt.subplot(2, 1, 1)
-for i in range(20):
-    plt.plot(x, beta.pdf(x, samp_A[i], samp_B[i]), color=blue)
+plt.plot(x, beta.pdf(x[:,None], samp_A[:20], samp_B[:20]), linewidth=1)
 plt.yticks(())
 plt.title('Posterior samples from the distribution of distributions '
                   'Beta($\\alpha$,$\\beta$)')
@@ -115,8 +114,7 @@ plt.title('Posterior samples from the distribution of distributions '
 # theta, and also the prior distribution for theta_j.
 # Plot this.
 plt.subplot(2, 1, 2)
-plt.plot(x, np.mean(beta.pdf(x, samp_A[:,None], samp_B[:,None]), axis=0),
-         linewidth=2, color=blue)
+plt.plot(x, np.mean(beta.pdf(x, samp_A[:,None], samp_B[:,None]), axis=0))
 plt.yticks(())
 plt.xlabel('$\\theta$', fontsize=20)
 plt.title('Predictive distribution for a new $\\theta$ '
@@ -130,10 +128,10 @@ x = np.linspace(0, 1, 250)
 # first plot separate model (same as above)
 plt.subplot(2, 1, 1)
 # note that for clarity only every 7th distribution is plotted
-for i in range(7, M-1, 7):
-    plt.plot(x, beta.pdf(x, y[i] + 1, n[i] - y[i] + 1), color=blue)
-# emphasise last sample
-plt.plot(x, beta.pdf(x, y[-1] + 1, n[-1] - y[-1] + 1), 'r', linewidth=2)
+plt.plot(x, beta.pdf(x[:,None], y[7:-1:7] + 1, n[7:-1:7] - y[7:-1:7] + 1),
+         linewidth=1)
+# highlight the last line
+plt.plot(x, beta.pdf(x, y[-1] + 1, n[-1] - y[-1] + 1), 'r')
 plt.yticks(())
 plt.title('separate model')
 
@@ -142,16 +140,21 @@ plt.title('separate model')
 # the other theta_j's.
 plt.subplot(2, 1, 2)
 # note that for clarity only every 7th distribution is plotted
-for i in range(7, M, 7):
-    plt.plot(
-        x,
-        np.mean(
-            beta.pdf(x, y[i] + samp_A[:,None], n[i] - y[i] + samp_B[:,None]),
-            axis=0
+lines = plt.plot(
+    x,
+    np.mean(
+        beta.pdf(
+            x[:,None],
+            y[7::7] + samp_A[:,None,None],
+            n[7::7] - y[7::7] + samp_B[:,None,None]
         ),
-        color = (blue if i != M-1 else 'r'),
-        linewidth = (1 if i != M-1 else 2),
-    )
+        axis=0
+    ),
+    linewidth=1,
+)
+# highlight the last line
+lines[-1].set_linewidth(2)
+lines[-1].set_color('r')
 plt.yticks(())
 plt.xlabel('$\\theta$', fontsize=20)
 plt.title('hierarchical model')
