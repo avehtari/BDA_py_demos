c7c7ca62bcbf8598429a3b4a286601c89e774d63
Tuomas Sivula <tuomas.sivula@aalto.fi>
2015-05-25 10:02:54 +0300

    Update demo5_2 and add notebook version

--- demos_ch5/demo5_2.py
+++ demos_ch5/demo5_2.py
@@ -23,15 +23,33 @@ y = np.array([28,  8, -3,  7, -1,  1, 18, 12])
 s = np.array([15, 10, 16, 11,  9, 11, 10, 18])
 M = len(y)
 
+# load the pre-computed results for the hierarchical model
+# replace this with your own code in Ex 5.1*
+hres = scipy.io.loadmat('demo5_2.mat')
+''' Content information of the precalculated results:
+>>> scipy.io.whosmat('demo5_2.mat')
+[('pxm', (8, 500),  'double'),
+ ('t',   (1, 1000), 'double'),
+ ('tp',  (1, 1000), 'double'),
+ ('tsd', (8, 1000), 'double'),
+ ('tm',  (8, 1000), 'double')]
+'''
+pxm = hres['pxm']
+t   = hres['t'][0]
+tp  = hres['tp'][0]
+tsd = hres['tsd']
+tm  = hres['tm']
 
-# plot separate and pooled model
+
+# plot separate, pooled and hierarchical model
 fig, axes = plt.subplots(3, 1, sharex=True, figsize=(8,10))
 x = np.linspace(-40, 60, 500)
 
 # separate
 lines = axes[0].plot(x, norm.pdf(x[:,None], y[1:], s[1:]), linewidth=1)
 line, = axes[0].plot(x, norm.pdf(x, y[0], s[0]), 'r')
-axes[0].legend((line, lines[1]), ('school A', 'other schools'))
+axes[0].legend((line, lines[1]), ('school A', 'other schools'),
+               loc='upper left')
 axes[0].set_yticks(())
 axes[0].set_title('separate model')
 
@@ -43,32 +61,47 @@ axes[1].plot(
         np.sum(y/s**2)/np.sum(1/s**2),
         np.sqrt(1/np.sum(1/s**2))
     ),
-    'r',
     label='All schools'
 )
-axes[1].legend()
+axes[1].legend(loc='upper left')
 axes[1].set_yticks(())
 axes[1].set_title('pooled model')
 
 # hierarchical
-# load the pre-computed results for the hierarchical model
-# replace this with your own code in Ex 5.1*
-hier_res = scipy.io.loadmat('demo5_2.mat')
-''' Content information of the precalculated results:
->>> scipy.io.whosmat('demo5_2.mat')
-[('pxm', (8, 500),  'double'),
- ('t',   (1, 1000), 'double'),
- ('tp',  (1, 1000), 'double'),
- ('tsd', (8, 1000), 'double'),
- ('tm',  (8, 1000), 'double')]
-'''
-lines = axes[2].plot(x, hier_res['pxm'][1:].T, linewidth=1)
-line, = axes[2].plot(x, hier_res['pxm'][0], 'r')
-axes[2].legend((line, lines[1]), ('school A', 'other schools'))
+lines = axes[2].plot(x, pxm[1:].T, linewidth=1)
+line, = axes[2].plot(x, pxm[0], 'r')
+axes[2].legend((line, lines[1]), ('school A', 'other schools'),
+               loc='upper left')
 axes[2].set_yticks(())
 axes[2].set_title('hierarchical model')
 axes[2].set_xlabel('Treatment effect')
 
+
+# plot various marginal and conditional posterior summaries
+fig, axes = plt.subplots(3, 1, sharex=True, figsize=(8,10))
+
+axes[0].plot(t, tp)
+axes[0].set_yticks(())
+axes[0].set_title('marginal posterior density $p(\\tau|y)$')
+axes[0].set_ylabel('$p(\\tau|y)$', fontsize=20)
+axes[0].set_xlim([0,35])
+
+lines = axes[1].plot(t, tm[1:].T, linewidth=1)
+line, = axes[1].plot(t, tm[0].T, 'r')
+axes[1].legend((line, lines[1]), ('school A', 'other schools'),
+               loc='upper left')
+axes[1].set_title('conditional posterior means of effects '
+                  '$\operatorname{E}(\\theta_j|\\tau,y)$')
+axes[1].set_ylabel('$\operatorname{E}(\\theta_j|\\tau,y)$', fontsize=20)
+
+lines = axes[2].plot(t, tsd[1:].T, linewidth=1)
+line, = axes[2].plot(t, tsd[0].T, 'r')
+axes[2].legend((line, lines[1]), ('school A', 'other schools'),
+               loc='upper left')
+axes[2].set_title('standard deviations of effects '
+                  '$\operatorname{sd}(\\theta_j|\\tau,y)$')
+axes[2].set_ylabel('$\operatorname{sd}(\\theta_j|\\tau,y)$', fontsize=20)
+axes[2].set_xlabel('$\\tau$', fontsize=20)
 
 plt.show()
 
