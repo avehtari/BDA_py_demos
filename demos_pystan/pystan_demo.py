8b6d514da639194bc1453eb3219cc71fb0e24109
Tuomas Sivula <tuomas.sivula@aalto.fi>
2015-12-10 10:09:55 +0200

    Move oddsratio computation to generated quantities.

--- demos_pystan/pystan_demo.py
+++ demos_pystan/pystan_demo.py
@@ -101,15 +101,15 @@ parameters {
   real<lower=0,upper=1> theta1;
   real<lower=0,upper=1> theta2;
 }
-transformed parameters {
-  real oddsratio;
-  oddsratio <- (theta2/(1-theta2))/(theta1/(1-theta1));
-}
 model {
   theta1 ~ beta(1,1);
   theta2 ~ beta(1,1);
   y1 ~ binomial(N1,theta1);
   y2 ~ binomial(N2,theta2);
+}
+generated quantities {
+  real oddsratio;
+  oddsratio <- (theta2/(1-theta2))/(theta1/(1-theta1));
 }
 """
 data = dict(N1=674, y1=39, N2=680, y2=22)
